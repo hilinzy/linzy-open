af4e25ba73a4f3db147ef9dcd777a59b882eeac0
lazy <707688808@qq.com>
2022-09-23 10:49:02 +0800

    no message

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "build ..."
 if [ "local_plugin" = $1 ] ; then
   mvn clean install -pl linzy-open-common,linzy-open-mongo -am
 elif [ "publish_plugin" = $1 ] ; then
-  mvn clean deploy -Prelease -pl linzy-open-common,linzy-open-mongo -am
+  mvn clean deploy -pl linzy-open-common,linzy-open-mongo -am
 fi
 echo "build done."
